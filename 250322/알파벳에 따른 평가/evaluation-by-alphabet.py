36fe0f9bcc71bc3e0112575cbcd0127dec06a5e5
YoungEE <134985583+Seonyoung33@users.noreply.github.com>
2025-03-22 13:31:20 +0900

    Title: 알파벳에 따른 평가, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250322/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+alp = str(input())
+
+if alp == "S":
+    print("Superior")
+elif alp == "A":
+    print("Excellent")
+elif alp == "B":
+    print("Good")
+elif alp == "C":
+    print("Usually")
+elif alp == "D":
+    print("Effort")
+else:
+    print("Failure")
