1991112c7419d3c5320f0c7dfee69ead7a1b40a1
YoungEE <134985583+Seonyoung33@users.noreply.github.com>
2025-03-23 11:55:04 +0900

    Title: 4번의 크기 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250323/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,14 @@
+a = int(input())
+bcde = input()
+
+array = bcde.split(" ")
+
+b = int(array[0])
+c = int(array[1])
+d = int(array[2])
+e = int(array[3])
+
+print(int(a>b))
+print(int(a>c))
+print(int(a>d))
+print(int(a>e))
