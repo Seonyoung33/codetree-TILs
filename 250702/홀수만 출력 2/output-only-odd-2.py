cacff8826a0f6ea7ed1ec0653f98d2497a3ed461
YoungEE <134985583+Seonyoung33@users.noreply.github.com>
2025-07-02 11:17:19 +0900

    Title: 홀수만 출력 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250702/홀수만 출력 2/output-only-odd-2.py
+++ 250702/홀수만 출력 2/output-only-odd-2.py
@@ -1,5 +1,5 @@
 a, b = map(int, input().split())
 
-for i in range(b, a-1, -1):
-    if i%2==1:
+for i in range(a, b - 1, -1):  # b부터 a까지 감소하면서 반복
+    if i % 2 == 1:             # 홀수인 경우만 출력
         print(i, end=" ")
