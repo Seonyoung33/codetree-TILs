67d889af2db03f6f38e1ba164929901c8d9f0635
YoungEE <134985583+Seonyoung33@users.noreply.github.com>
2025-09-24 17:13:47 +0900

    Title: 배수의 개수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250924/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,12 @@
+three_cnt = 0
+five_cnt = 0
+
+for i in range(10):
+    num = int(input())
+
+    if num%3==0:
+        three_cnt += 1
+    if num%5==0:
+        five_cnt += 1
+    
+print(three_cnt, five_cnt, end=' ')
